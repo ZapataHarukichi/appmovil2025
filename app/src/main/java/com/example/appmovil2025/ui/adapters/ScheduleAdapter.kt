abe657e46888cd72bf37cb550a6f2d93160b3068
Zapata Carbonel Harold Andre <zapatacarbonel@gmail.com>
2025-06-05 01:35:53 -0500

    añadir viewBinding

---
+++ app/src/main/java/com/example/appmovil2025/ui/adapters/ScheduleAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.appmovil2025.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appmovil2025.R
+import com.example.appmovil2025.ui.model.ScheduleItem
+
+class ScheduleAdapter(private val scheduleList: List<ScheduleItem>):
+    RecyclerView.Adapter<ScheduleAdapter.ScheduleViewHolder>(){
+
+    class ScheduleViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val tvTime: TextView = itemView.findViewById(R.id.TVHora)
+        val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
+        val tvSpeaker: TextView = itemView.findViewById(R.id.tvSpeaker)
+        val tvTag: TextView = itemView.findViewById(R.id.tvTag)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduleViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_schedule_fragment, parent, false)
+        return ScheduleViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ScheduleViewHolder, position: Int) {
+        val item = scheduleList[position]
+        holder.tvTime.text = item.time
+        holder.tvTitle.text = item.title
+        holder.tvSpeaker.text = item.speaker
+        holder.tvTag.text = item.tag
+    }
+
+    override fun getItemCount(): Int = scheduleList.size
+}
---
+++ app/src/main/java/com/example/appmovil2025/ui/model/ScheduleItem.kt
@@ -0,0 +1,8 @@
+package com.example.appmovil2025.ui.model
+
+data class ScheduleItem(
+    val time: String,
+    val title: String,
+    val speaker: String,
+    val tag: String
+)
